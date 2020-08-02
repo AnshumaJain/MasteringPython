3234b72fde243006e93e6d337108dffc3d7f8640
Anshuma Jain <anshumajain9@gmail.com>
2020-08-02 00:02:58 -0700

    cleaned up the code

--- Compound_Interest.py
+++ Compound_Interest.py
@@ -1,14 +1,16 @@
 
-P = float(input("Enter the Principle amount = "))
-R = float(input("Enter the Rate of Interest = "))
-t = float(input("Enter the Time of deposit = "))
 
-def CI(P, R, t):
+def CI(principle, rate, time):
+    ci = principle * pow(1 + (rate/100), time)
+    return str(ci)
 
-    ci = P * (pow((1 + (R/100)), t))
-    print "Coumpound_Interest =", str(ci)
+if __name__ == "__main__":
 
-CI(P,R,t)
+    P = float(input("Enter the Principle amount: "))
+    R = float(input("Enter the Rate of Interest: "))
+    t = float(input("Enter the Time of deposit: "))
+
+    print("Compound Interest: ", CI(P,R,t))
 
 
 
