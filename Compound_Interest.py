fc74c843f84600b58a7c8205ff162155b9c35e48
Anshuma Jain <anshumajain9@gmail.com>
2020-08-02 13:25:32 -0700

    fixed PEP8

--- Compound_Interest.py
+++ Compound_Interest.py
@@ -4,6 +4,7 @@ def CI(principle, rate, time):
     ci = principle * pow(1 + (rate/100), time)
     return str(ci)
 
+
 if __name__ == "__main__":
 
     P = float(input("Enter the Principle amount: "))
