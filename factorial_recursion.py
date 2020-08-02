0d6fa9cbc9329c4e4cde9d4f5a1dfcc9208789c1
Anshuma Jain <anshumajain9@gmail.com>
2020-08-02 10:32:02 -0700

    cleaned up the code

--- factorial_recursion.py
+++ factorial_recursion.py
@@ -1,13 +1,13 @@
+# Implement factorial using recursion
 
 def factorial_recursion(num):
-
     if num != 1:
         return num * factorial_recursion(num-1)
     else:  # Base case
-        return 1  # 1! = 1
+        return 1  # since 1! = 1
 
 
 if __name__ == "__main__":
 
-    n = int(input("Enter a positive integer number : "))
+    n = int(input("Enter a positive integer number: "))
     print("Factorial of", n, ":", factorial_recursion(n))
