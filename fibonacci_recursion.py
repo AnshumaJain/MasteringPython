53f935de4330d5c750d5f2755445f9c1145ae1b5
Anshuma Jain <anshumajain9@gmail.com>
2020-08-02 10:36:25 -0700

    cleaned up the code

--- fibonacci_recursion.py
+++ fibonacci_recursion.py
@@ -1,19 +1,17 @@
+# Print the fibonacci sum for a given number using recursion
 
-def fibonacci_recursion(n):
+def fibonacci_recursion(num):
 
     # Base case
-    if n == 1:  # Fib(1) = 1
+    if num == 1:  # fib(1) = 1
         return 1
-    elif n == 0:  # Fib(0) = 0
+    elif num == 0:  # fib(0) = 0
         return 0
 
-    return fibonacci_recursion(n-1) + fibonacci_recursion(n-2)
+    return fibonacci_recursion(num-1) + fibonacci_recursion(num-2)
 
 
 if __name__ == "__main__":
 
-    n = int(input("Enter a positive integer number : "))
-    print("Fibonacci sum for the given number : ", fibonacci_recursion(n))
-
-
-
+    n = int(input("Enter a positive integer number: "))
+    print("Fibonacci sum for the given number: ", fibonacci_recursion(n))
