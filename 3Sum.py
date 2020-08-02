86352cb59c78b28f88a65048dd6caa4de7ccf9b1
Anshuma Jain <anshumajain9@gmail.com>
2020-08-01 23:51:50 -0700

    Update 3Sum.py
    cleaned up the code

--- 3Sum.py
+++ 3Sum.py
@@ -1,23 +1,25 @@
-def threeSum(nums):
+"""
+Given an array nums of n integers, are there elements a, b, c in nums
+such that a + b + c = 0? Find all unique triplets in the array
+which gives the sum of zero.
+"""
 
-    set = []
-    h = {}
-    for i in nums:
-        a = i
-        for j, c in enumerate(nums):
-            b = - (a + c)
-            if a != c and b != c and a != b:
-                if b not in h:
-                    h[c] = j
+
+def three_sum(nums):
+    my_set = []
+    my_dict = {}
+    for a in nums:  # for element 'a' in nums
+        for index, c in enumerate(nums):  # for element 'c' in nums
+            b = - (a + c)  # calculate value of 'b' to make a+b+c=0
+            if a != c and b != c and a != b:  # if a,b,c are different
+                if b not in my_dict:
+                    my_dict[c] = index
                 else:
-                    list = []
-                    list.append(a)
-                    list.append(b)
-                    list.append(c)
-                    set.append(list)
-    print("dict = ", h)
-    print("list = ", list)
-    return set
+                    list = [a, b, c]
+                    #print("list = ", list)
+                    my_set.append(list)
 
-print(threeSum([-1, 0, 1, 2, -1, -4]))
+    print("dict = ", my_dict)
+    return my_set
 
+print(three_sum([-1, 0, 1, 2, -1, -4]))
