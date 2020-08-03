299d07cf5369fd3bec41311f82a8959283d9743d
Anshuma Jain <anshumajain9@gmail.com>
2020-08-03 08:42:20 -0700

    cleaned up code; fixed PEP8

--- twoSumUsingHashtable.py
+++ twoSumUsingHashtable.py
@@ -1,14 +1,30 @@
+"""
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+Example:
+Given nums = [2, 7, 11, 15], target = 9,
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1]
+"""
+from typing import List
+
 
 class Solution:
-    """
-    Given an array of integers, return indices of the two numbers
-    such that they add up to a specific target.
-    """
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        h = {}
+
+    def two_sum(self, nums: List[int], target: int) -> List[int]:
+
+        hash_map = {}
         for i, num in enumerate(nums):
             n = target - num
-            if n not in h:
-                h[num] = i
+            if n not in hash_map:
+                hash_map[num] = i
             else:
-                return [h[n], i]
+                return [hash_map[n], i]
+
+
+if __name__ == "__main__":
+    nums = [2, 7, 11, 15]
+    target = 9
+    sol = Solution()
+    print(sol.two_sum(nums, target))
