76ceab453c434293e0a987f241360171ac7a512d
Anshuma Jain <anshumajain9@gmail.com>
2020-08-03 08:19:34 -0700

    cleaned up the code

--- Palindrome_check.py
+++ Palindrome_check.py
@@ -1,16 +1,20 @@
+# Find out if the given input string is a palindrome
 
-input_string = input("Enter the string = ")
-size = len(input_string)
-ln = size-1
+def is_palindrome(input_string):
 
-for i in range(0, ln):
-    if input_string[i] == input_string[ln-i] and i != (ln-i - 1) and i != (ln-i):
-        pass
-    elif input_string[i] == input_string[ln-i]:
-        print("it's a palindrome")
-        break
-    else:
-        print("it's not a palindrome")
-        break
+    size = len(input_string)
+    ln = size - 1
+    for i in range(0, ln):
+        if input_string[i] == input_string[ln-i] and i != (ln-i - 1) and i != (ln-i):
+            pass
+        elif input_string[i] == input_string[ln-i]:
+            print("it's a palindrome")
+            break
+        else:
+            print("it's not a palindrome")
+            break
 
 
+if __name__ == "__main__":
+    input_string = input("Enter the string = ")
+    is_palindrome(input_string)
