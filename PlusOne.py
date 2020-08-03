1ac34ae33ac72a707039b5cbb49630a5b48a0632
Anshuma Jain <anshumajain9@gmail.com>
2020-08-03 00:36:28 -0700

    added comments; misc cleanup

--- PlusOne.py
+++ PlusOne.py
@@ -1,4 +1,23 @@
-def plusOne(digits):
+"""
+Given a non-empty array of digits representing a non-negative integer,
+ increment one to the integer. The digits are stored such that the most
+ significant digit is at the head of the list, and each element in the
+ array contains a single digit. You may assume the integer does not
+ contain any leading zero, except the number 0 itself.
+
+Example 1:
+Input: [1,2,3]
+Output: [1,2,4]
+Explanation: The array represents the integer 123.
+
+Example 2:
+Input: [4,3,2,1]
+Output: [4,3,2,2]
+Explanation: The array represents the integer 4321.
+"""
+
+
+def plus_one(digits):
     i = len(digits) - 1
     while 1:
         if digits[i] < 9:
@@ -11,4 +30,5 @@ def plusOne(digits):
             else:
                 return ([1] + digits)
 
-print(plusOne([9,9,9,9]))
+if __name__ == "__main__":
+    print(plus_one([9, 9, 9, 9]))
