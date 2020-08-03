15e1f66c4355c9942b216f2f6a16ebe2d11611ca
Anshuma Jain <anshumajain9@gmail.com>
2020-08-03 08:09:12 -0700

    fixed to run on python3

--- ListSecondMax.py
+++ ListSecondMax.py
@@ -1,31 +1,38 @@
+# Find the maximum and second maximum value given a list
 
-input_list = input("Enter the list =")
+def find_second_max_in_list(input_list):
 
-size = len(input_list)
-i = 0
-max = 0
-temp = 0
-Sec_max = 0
+    size = len(input_list)
+    max_val = input_list[0]
+    temp = 0
+    sec_max_val = input_list[0]
 
-for i in range(0, size):
+    for i in range(0, size):
 
-    if input_list[i] >= max:
-        temp = max
-        max = input_list[i]
-    else:
-        if input_list[i] > Sec_max:
-            Sec_max = input_list[i]
+        if input_list[i] >= max_val:
+            temp = max_val
+            max_val = input_list[i]
+        else:
+            if input_list[i] > sec_max_val:
+                sec_max_val = input_list[i]
 
-    if temp > Sec_max and temp != max:
-        Sec_max = temp
+        if temp > sec_max_val and temp != max_val:
+            sec_max_val = temp
 
-print "First max no. is " + str(max)
-print "Second max no. is " + str(Sec_max)
+    return max_val, sec_max_val
 
-# [15,2,5,6,3]-
-# [1,2,5,6,3]-
-# [1,2,3,4,100,5,200]-
-# [10,5,20]-
-# [10,20,10,20]-
-# [1,2,1,2]-
-# [10,10,1,1]-
+
+if __name__ == "__main__":
+
+    # [15,2,5,6,3]-
+    # [1,2,5,6,3]-
+    # [1,2,3,4,100,5,200]-
+    # [10,5,20]-
+    # [10,20,10,20]-
+    # [1,2,1,2]-
+    # [10,10,1,1]-
+    input_list = eval(input("Enter the list ="))
+
+    max_val, sec_max_val = find_second_max_in_list(input_list)
+    print("First max no. is:", str(max_val))
+    print("Second max no. is:", str(sec_max_val))
