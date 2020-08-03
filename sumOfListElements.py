b0c606df9b00a4cd5176887bc40ed8c1767bacf5
Anshuma Jain <anshumajain9@gmail.com>
2020-08-03 08:49:39 -0700

    cleaned up the code

--- sumOfListElements.py
+++ sumOfListElements.py
@@ -1,10 +1,15 @@
+# Return the sum of all the values in the given input list
 
-input_list = eval(input("Enter the list to be added ="))
-sum = 0
+def list_sum(input_list):
+        sum = 0
+        for i in range(0, len(input_list)):
+                sum = sum + input_list[i]
 
-for i in range(0, len(input_list)):
-        sum = sum + input_list[i]
+        return sum
 
-print(sum)
 
-# [1,2,3], [4, 5, 6, 10]
+if __name__ == "__main__":
+
+        # [1,2,3], [4, 5, 6, 10]
+        input_list = eval(input("Enter the list to be added ="))
+        print(list_sum(input_list))
