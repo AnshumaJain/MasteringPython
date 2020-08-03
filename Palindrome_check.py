b3724bf7a4b6f7e5d7c2d16209c2cae8d80e22a6
Anshuma Jain <anshumajain9@gmail.com>
2020-08-03 08:16:29 -0700

    fixed to run on python3. fixed per PEP8.

--- Palindrome_check.py
+++ Palindrome_check.py
@@ -1,16 +1,16 @@
 
 input_string = input("Enter the string = ")
 size = len(input_string)
-a = size-1
+ln = size-1
 
-for i in range(0, a):
-    if input_string[i] == input_string[a-i] and i != (a-i - 1) and i != (a-i):
+for i in range(0, ln):
+    if input_string[i] == input_string[ln-i] and i != (ln-i - 1) and i != (ln-i):
         pass
-    elif input_string[i] == input_string[a-i]:
-        print "it's a palindrome"
+    elif input_string[i] == input_string[ln-i]:
+        print("it's a palindrome")
         break
     else:
-        print "it's not a palindrome"
+        print("it's not a palindrome")
         break
 
 
