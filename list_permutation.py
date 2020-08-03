5de13814d671c55dfe530361d7ff79dcb9b65c97
Anshuma Jain <anshumajain9@gmail.com>
2020-08-02 23:24:29 -0700

    cleaned up the code and fixed a minor bug

--- list_permutation.py
+++ list_permutation.py
@@ -1,26 +1,41 @@
+"""
+LeetCode Problem # 46: Permutations
+Given a collection of distinct integers, return all possible permutations.
+Example:
+Input: [1,2,3]
+Output:
+[
+  [1,2,3],
+  [1,3,2],
+  [2,1,3],
+  [2,3,1],
+  [3,1,2],
+  [3,2,1]
+]
+"""
+from typing import List
 
 
-def permute(nums):
+def permute(nums: List[int]) -> List[List[int]]:
 
     n = len(nums)
     if n == 0:
-        return nums
+        return [nums]
     elif n == 1:
-        return nums
+        return [nums]
     elif n == 2:
-        return (nums, nums[::-1])
+        return [nums, nums[::-1]]
 
-    mynums =[]
-
+    my_nums = []
     for i in range(0, n):
         for j in permute(nums[0:i] + nums[(i+1):n]):
-            mynums.append([nums[i]] + j)
+            my_nums.append([nums[i]] + j)
 
-    return mynums
+    return my_nums
 
 
+if __name__ == "__main__":
+    a = permute([1, 2, 3, 4])
 
-a = (permute([1,2,3,4]))
-
-for i in a:
-    print(i)
+    for i in a:
+        print(i)
