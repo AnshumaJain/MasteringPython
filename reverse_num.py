24f867cc41493d510eb770f26b63baa4382bda01
Anshuma Jain <anshumajain9@gmail.com>
2020-08-03 08:31:46 -0700

    fixed to handle negative inputs

--- reverse_num.py
+++ reverse_num.py
@@ -1,12 +1,19 @@
+
 def reverse(x: int) -> int:
+
+    sign = 1
+    if x < 0:
+        x = x*-1
+        sign = -1
+
     rev = 0
     while x != 0:
         pop = x % 10
         x //= 10
-
+        print(x)
         temp = rev * 10 + pop
         rev = temp
 
-    return rev
+    return sign*rev
 
 print(reverse(-123))
