87b7c16295470dded070bf6a4506338c5a55ca6e
Anshuma Jain <anshumajain9@gmail.com>
2020-08-03 08:34:15 -0700

    cleaned up code; fixed PEP8

--- reverse_num.py
+++ reverse_num.py
@@ -1,19 +1,22 @@
+# Reverse the digits in the given input integer
 
-def reverse(x: int) -> int:
+def reverse_num(num: int) -> int:
 
     sign = 1
-    if x < 0:
-        x = x*-1
+    if num < 0:
+        num = num*-1
         sign = -1
 
     rev = 0
-    while x != 0:
-        pop = x % 10
-        x //= 10
-        print(x)
+    while num != 0:
+        pop = num % 10
+        num //= 10
         temp = rev * 10 + pop
         rev = temp
 
     return sign*rev
 
-print(reverse(-123))
+
+if __name__ == "__main__":
+
+    print(reverse_num(-123))
