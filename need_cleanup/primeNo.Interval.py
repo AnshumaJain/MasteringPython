f0a5602020722e42e26e5a08e7c9c75af0d5ac9d
Anshuma Jain <anshumajain9@gmail.com>
2020-08-04 03:14:12 -0700

    update from python 2 ->3; added comments; fixed PEP8; cleaned up code;

--- need_cleanup/primeNo.Interval.py
+++ need_cleanup/primeNo.Interval.py
@@ -1,32 +1,29 @@
-
+# List all prime numbers between the specified start and end intervals
 import math
 
-start = int(input(' Enter the interval from ='))
-end = int(input('to='))
+start = int(input(' Enter the interval from: '))
+end = int(input('to: '))
 
-List_prime = [2]             #list of prime no.s
-length = len(List_prime)
+list_prime = [2]  # list of prime numbers
+length = len(list_prime)
+check_prime = None
 
 for i in range(3, end):
-
     for j in range(0, length):
-
-        Check_prime = i % List_prime[j]
-
-        if Check_prime == 0:
+        check_prime = i % list_prime[j]
+        if check_prime == 0:
             break
 
-    if j == (length - 1) and Check_prime != 0:
-
-        List_prime.append(i)
+    if j == (length - 1) and check_prime != 0:
+        list_prime.append(i)
         length += 1
 
 subset_list = []
 
 for k in range(start, end):
-    if k in List_prime : #learned new
+    if k in list_prime:
         subset_list.append(k)
 
-print subset_list
+print(subset_list)
 #print List_prime
 #print list_prime.range (start,end)
