1b25abf883a020c12dd2762795f703822fe37371
Anshuma Jain <anshumajain9@gmail.com>
2020-08-04 00:53:47 -0700

    fixed to run on python3; cleaned up code; fixed PEP8

--- SecondMaxValueOfList.py
+++ SecondMaxValueOfList.py
@@ -1,41 +1,43 @@
-input_list = input("Enter the list =")
+# Find the maximum and second maximum value given a list
 
-size = len(input_list)
-i = 0
-max_prev = 0
-max = 0
-temp_prev = 0
-temp = 0
-Sec_max = 0
+def find_second_max_in_list(input_list):
+
+    size = len(input_list)
+    temp = 0
+    temp_prev = 0
+    max_val = input_list[0]
+    max_prev = 0
+    sec_max_val = input_list[0]
+
+    for i in range(0, size):
+
+        if input_list[i] > max_val:
+            max_prev = max_val
+            max_val = input_list[i]
+        else:
+            temp_prev = temp
+            temp = input_list[i]
+
+        if temp > temp_prev and sec_max_val <= temp:
+            sec_max_val = temp
+        elif temp_prev > max_prev and sec_max_val <= temp_prev:
+            sec_max_val = temp_prev
+        elif sec_max_val <= max_prev:
+            sec_max_val = max_prev
+
+    return max_val, sec_max_val
 
 
-for i in range(0, size):
+if __name__ == "__main__":
 
-    if input_list[i] > max:
-        max_prev = max
-        max = input_list[i]
-    else:
-        temp_prev = temp
-        temp = input_list[i]
+    # [1,2,5,6,3]
+    # [1,2,3,4,100,5,200]
+    # [10,5,20]
+    # [10,20,10,20]
+    # [1,2,1,2]
+    # [10,10,1,1]
+    input_list = eval(input("Enter the list: "))
 
-    if temp > temp_prev and Sec_max <= temp:
-        Sec_max = temp
-    elif temp_prev > max_prev and Sec_max <= temp_prev:
-        Sec_max = temp_prev
-    elif Sec_max <= max_prev:
-        Sec_max = max_prev
-
-
-    print max_prev, max, temp_prev, temp, Sec_max
-
-
-print "secondmax" + str(Sec_max)
-print "max" + str(max)
-#print "temp" + str(temp)
-
-#  [1,2,5,6,3]
-# [1,2,3,4,100,5,200]
-# [10,5,20]
-# [10,20,10,20]
-[1,2,1,2]
-[10,10,1,1]
+    max_val, sec_max_val = find_second_max_in_list(input_list)
+    print("Second Max: ", str(sec_max_val))
+    print("Max: ", str(max_val))
