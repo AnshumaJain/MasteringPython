507bfc9b707be9f2fbb7e581e03456929d0cb4fd
Anshuma Jain <anshumajain9@gmail.com>
2020-08-05 00:01:54 -0700

    added comments; fixed PEP8; cleaned up code;

--- sorting/Shell_sort.py
+++ sorting/Shell_sort.py
@@ -8,41 +8,37 @@ Output: [1,2,3,5]
 from typing import List
 
 
-class Solution:
+def shell_sort(nums: List[int]) -> List[int]:
 
-    @staticmethod
-    def sort_array(nums: List[int]) -> List[int]:
+    ln = len(nums)
+    for j in range(1, ln - 1):
+        n = (ln // (2 ** j))
 
-        ln = len(nums)
+        for i in range(0, ln):
+            if (i + n) < ln and nums[i] >= nums[i + n]:
+                nums[i], nums[i + n] = nums[i + n], nums[i]
 
-        for j in range(1, ln - 1):
-            n = (ln // (2 ** j))
+                my_indices = list(range(0, i + 1))
+                my_indices.reverse()
+                for k in my_indices:
+                    if nums[k] <= nums[k - n] and k >= n:
+                        nums[k - n], nums[k] = nums[k], nums[k - n]
 
-            for i in range(0, ln):
+    return nums
 
-                if (i + n) < ln and nums[i] >= nums[i + n]:
-                    nums[i], nums[i + n] = nums[i + n], nums[i]
 
-                    my_indices = list(range(0, i + 1))
-                    my_indices.reverse()
-                    for k in my_indices:
-                        if nums[k] <= nums[k - n] and k >= n:
-                            nums[k - n], nums[k] = nums[k], nums[k - n]
+if __name__ == "__main__":
 
-        return nums
+    test_array = [
+        [15, 2, 5, 6, 3],
+        [1, 2, 5, 6, 4, 3],
+        [10, 10, 1, 1],
+        [10, 5, 20],
+        [1, 2, 3, 4, 100, 5, 200],
+        [10, 20, 10, 20],
+        [1, -2, 1, 2],
+    ]
 
-test_array = [
-            [15, 2, 5, 6, 3],
-            [1, 2, 5, 6, 4, 3],
-            [10, 10, 1, 1],
-            [10, 5, 20],
-            [1, 2, 3, 4, 100, 5, 200],
-            [10, 20, 10, 20],
-            [1, -2, 1, 2],
-        ]
-
-obj = Solution()
-for l in test_array:
-    op = obj.sort_array(l)
-    print(op)
-
+    for unsorted_list in test_array:
+        op = shell_sort(unsorted_list)
+        print(op)
