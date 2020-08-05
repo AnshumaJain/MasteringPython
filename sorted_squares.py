989daa7df22c452837b6e8f5ab331f97c19f0697
Anshuma Jain <anshumajain9@gmail.com>
2020-08-04 22:21:31 -0700

    updated comment

--- sorted_squares.py
+++ sorted_squares.py
@@ -1,4 +1,5 @@
 """
+LeetCode Problem #977: Squares of a Sorted Array
 Given an array of integers A sorted in non-decreasing order, return an
 array of the squares of each number, also in sorted non-decreasing order.
 
