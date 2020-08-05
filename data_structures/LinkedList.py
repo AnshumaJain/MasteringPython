ae02d53405b44a0550c47f44d47695681cd6477e
Anshuma Jain <anshumajain9@gmail.com>
2020-08-04 23:12:05 -0700

    added comments; fixed PEP8; cleaned up code;

--- data_structures/LinkedList.py
+++ data_structures/LinkedList.py
@@ -1,56 +1,57 @@
+# Implement a Linked List in python with add(), find(), remove() functionalities
 
-class Node():
-
-    def __init__(self, data, nextNodeAdd):
+class Node:
+    def __init__(self, data, next_node_add):
         self.data = data
-        self.nextNodeAdd = nextNodeAdd
+        self.next_node_add = next_node_add
 
-class LinkedList():
 
+class LinkedList:
     def __init__(self):
         self.root = None
         self.size = 0
 
-    def Print(self):
+    def print(self):
         value = self.root
         while value:
-            print(value.data)
-            value = value.nextNodeAdd
+            print(value.data, " ", end="")  # suppress newline using 'end'
+            value = value.next_node_add
+        print("")
 
-    def Add(self, num):
+    def add(self, num):
         node = Node(num, self.root)
         self.root = node
         self.size += 1
 
-    def Find(self, num):
+    def find(self, num):
         ptr = self.root
         while ptr:
             if num == ptr.data:
                 return True
             else:
-                ptr = ptr.nextNodeAdd
+                ptr = ptr.next_node_add
         return False
 
-    def Remove(self, num):
+    def remove(self, num):
         ptr = self.root
+        prev_ptr = None
         while ptr:
             if num == ptr.data:
-                Prevptr.nextNodeAdd = ptr.nextNodeAdd
+                prev_ptr.next_node_add = ptr.next_node_add
                 break
             else:
-                Prevptr = ptr
-                ptr = ptr.nextNodeAdd
+                prev_ptr = ptr
+                ptr = ptr.next_node_add
         return False
 
 
 p = LinkedList()
-p.Add(3)
-p.Add(6)
-p.Add(7)
-p.Add(2)
-p.Print()
-print(p.Remove(8))
-print(p.Find(6))
-
-
-
+p.add(3)
+p.add(6)
+p.add(7)
+p.add(2)
+p.print()
+print(p.remove(8))
+print(p.find(6))
+print(p.remove(7))
+p.print()
