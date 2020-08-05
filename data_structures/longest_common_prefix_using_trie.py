35510436a0a09d60159c41db10d7ce4286a3c149
Anshuma Jain <anshumajain9@gmail.com>
2020-08-04 22:53:21 -0700

    added comments; added main(); cleaned up the code

--- data_structures/longest_common_prefix_using_trie.py
+++ data_structures/longest_common_prefix_using_trie.py
@@ -1,9 +1,28 @@
+"""
+LeetCode Problem #14: Longest Common Prefix
+
+Write a function to find the longest common prefix string amongst an array of strings.
+If there is no common prefix, return an empty string "".
+
+Example 1:
+Input: ["flower","flow","flight"]
+Output: "fl"
+
+Example 2:
+Input: ["dog","racecar","car"]
+Output: ""
+Explanation: There is no common prefix among the input strings.
+
+Note:
+All given inputs are in lowercase letters a-z.
+"""
+
+
 class Solution:
     def __init__(self):
         """
-        Initialize your data structure here.
+        Initializes the data structure.
         """
-
         self.head = {}
 
     def insert(self, strs):
@@ -17,14 +36,14 @@ class Solution:
                     cur[ch] = {}
                 cur = cur[ch]
             cur['*'] = True
-        print(self.head)
+        # print(self.head)
         return strs
 
-    def longestCommonPrefix(self, strs) -> str:
+    def longest_common_prefix(self, strs) -> str:
         self.insert(strs)
         prefix = ""
         cur = self.head
-        if strs == []:
+        if not strs:
             return prefix
         while 1:
             for ch in cur:
@@ -32,12 +51,11 @@ class Solution:
                     return prefix
                 else:
                     prefix = prefix + ch
-
             cur = cur[ch]
-
         return prefix
 
 
-p = Solution()
-
-print(p.longestCommonPrefix([]))
+if __name__ == "__main__":
+    p = Solution()
+    print("Longest prefix:", p.longest_common_prefix(["flower", "flow", "flight"]))
+    print("Longest prefix:", p.longest_common_prefix(["dog", "racecar", "car"]))
