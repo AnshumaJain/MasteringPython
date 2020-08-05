24b04329cd21f5df4459503bb9ca5a5aeca26c83
Anshuma Jain <anshumajain9@gmail.com>
2020-08-04 22:39:19 -0700

    added comments; added main(); fixed PEP8; cleaned up code

--- data_structures/trie.py
+++ data_structures/trie.py
@@ -1,14 +1,31 @@
+"""
+LeetCode Problem #208: Implement Trie (Prefix Tree)
+
+Implement a trie with insert, search, and startsWith methods.
+
+Example:
+Trie trie = new Trie();
+trie.insert("apple");
+trie.search("apple");   // returns true
+trie.search("app");     // returns false
+trie.startsWith("app"); // returns true
+trie.insert("app");
+trie.search("app");     // returns true
+
+Note:
+You may assume that all inputs are consist of lowercase letters a-z.
+All inputs are guaranteed to be non-empty strings.
+"""
+
 class Trie:
 
     def __init__(self):
         """
-        Initialize your data structure here.
+        Initializes the data structure.
         """
-
         self.head = {}
 
-
-    def insert(self, word: str) -> None:
+    def insert(self, word: str) -> str:
         """
         Inserts a word into the trie.
         """
@@ -18,7 +35,7 @@ class Trie:
                 cur[ch] = {}
             cur = cur[ch]
         cur['*'] = True
-        print(self.head)
+        # print(self.head)
         return word
 
     def search(self, word: str) -> bool:
@@ -36,8 +53,7 @@ class Trie:
         else:
             return False
 
-
-    def startsWith(self, prefix: str) -> bool:
+    def starts_with(self, prefix: str) -> bool:
         """
         Returns if there is any word in the trie that starts with the given prefix.
         """
@@ -48,15 +64,19 @@ class Trie:
             cur = cur[ch]
         return True
 
-p = Trie()
-# print(p.insert("hell"))
-# print(p.insert("hello"))
-print(p.insert("flower"))
-print(p.insert("flow"))
 
-print(p.insert("flight"))
-# print(p.search("hello"))
-# print(p.search("hell"))
-# print(p.search("he"))
-# print(p.search("helloi"))
-# print(p.startsWith("\\"))
+if __name__ == "__main__":
+    p = Trie()
+    p.insert("hell")
+    p.insert("hello")
+    p.insert("flower")
+    p.insert("flow")
+    p.insert("flight")
+
+    print(p.search("hello"))
+    print(p.search("hell"))
+    print(p.search("he"))
+    print(p.search("helloi"))
+    print(p.starts_with("hell"))
+    print(p.starts_with("fle"))
+    print(p.starts_with("\\"))
