354aaee9046d5935f04fff351be117440fd75db6
Anshuma Jain <anshumajain9@gmail.com>
2020-08-06 23:05:05 -0700

    updated comment; cleaned up code;

--- General/reverse_num.py
+++ General/reverse_num.py
@@ -1,22 +1,50 @@
-# Reverse the digits in the given input integer
+"""
+LeetCode Problem #7: Reverse Integer
 
-def reverse_num(num: int) -> int:
+Given a 32-bit signed integer, reverse digits of an integer.
 
-    sign = 1
+Example 1:
+Input: 123
+Output: 321
+
+Example 2:
+Input: -123
+Output: -321
+
+Example 3:
+Input: 120
+Output: 21
+
+Note:
+Assume we are dealing with an environment which could only store integers within
+the 32-bit signed integer range: [−231,  231 − 1]. For the purpose of this problem,
+assume that your function returns 0 when the reversed integer overflows.
+"""
+
+
+def reverse(num: int) -> int:
+
+    negative = False
     if num < 0:
-        num = num*-1
-        sign = -1
+        negative = True
+        num *= -1
 
     rev = 0
-    while num != 0:
+    while num:
         pop = num % 10
         num //= 10
-        temp = rev * 10 + pop
-        rev = temp
+        rev = rev * 10 + pop
 
-    return sign*rev
+    if negative:
+        rev *= -1
+
+    if rev > (2**31) - 1 or rev < -2**31:
+        return 0
+
+    return rev
 
 
 if __name__ == "__main__":
-
-    print(reverse_num(-123))
+    print(reverse(-123))
+    print(reverse(123))
+    print(reverse(120))
